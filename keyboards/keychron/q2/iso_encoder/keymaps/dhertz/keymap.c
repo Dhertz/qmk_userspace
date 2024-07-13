44f012f3d94469d573863a80c0a2e34fcd27f303
Dan Hertz <1764702+Dhertz@users.noreply.github.com>
2024-07-13 16:46:49 -0700

    use dip to chose when to send NUBS or GRV

--- keyboards/keychron/q2/iso_encoder/keymaps/dhertz/keymap.c
+++ keyboards/keychron/q2/iso_encoder/keymaps/dhertz/keymap.c
@@ -4,6 +4,13 @@
 #include QMK_KEYBOARD_H
 #include "dhertz.h"
 
+static bool dip_switch_state[NUM_DIP_SWITCHES] = {0};
+
+enum keyboard_keycodes {
+    GRV_NUBS_DIP = NEW_SAFE_RANGE,
+    NUBS_GRV_DIP,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default Layer
      * ,---------------------------------------------------------------.
@@ -31,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |---------------------------------------------------------------|
      * |ClTab|   |   |   |   |   |   |   |   |   |   |   |   |ScnSh|   |
      * |------------------------------------------------------`    |---|
-     * |Mke/Fl|   |   |   |   |   |   |   |   |CSL|   |   |  `|    |   |
+     * |Mke/Fl|   |   |   |   |   |   |   |   |CSL|   |   |  `|    |End|
      * |---------------------------------------------------------------|
      * |    |  `|   |   |CAC|   |   |   |   |   |   |   |      |PgU|   |
      * |---------------------------------------------------------------|
@@ -39,13 +46,57 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------'
      */
   [1] = LAYOUT_iso_68(
-      KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F10, KC_F11, KC_TRNS, SLEEP_OSX,
+      NUBS_GRV_DIP, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F10, KC_F11, KC_TRNS, SLEEP_OSX,
       CTL_TAB_CTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
       QK_MAKE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, CMD_SFT_L, KC_TRNS, KC_TRNS, KC_NUBS, SCRNSHT, KC_END,
-      KC_TRNS, KC_NUBS, KC_TRNS, KC_TRNS, CMD_ALT_C, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP,
+      KC_TRNS, GRV_NUBS_DIP, KC_TRNS, KC_TRNS, CMD_ALT_C, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP,
       KC_LCTL, KC_TRNS, CMD_SFT_LBR_CMD, KC_TRNS, CMD_SFT_RBR_CMD, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END
       ),
 };
+
+bool dip_switch_update_user(uint8_t index, bool active) {
+    dip_switch_state[index] = active;
+    // do not continue (set default layer to be 1)
+    return false;
+}
+
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode) {
+        case GRV_NUBS_DIP:
+            if (dip_switch_state[0]) {
+                if (record->event.pressed) {
+                    register_code(KC_GRV);
+                } else {
+                    unregister_code(KC_GRV);
+                }
+            } else {
+                if (record->event.pressed) {
+                    register_code(KC_NUBS);
+                } else {
+                    unregister_code(KC_NUBS);
+                }
+            }
+            break;
+        case NUBS_GRV_DIP:
+            if (dip_switch_state[0]) {
+                if (record->event.pressed) {
+                    register_code(KC_NUBS);
+                } else {
+                    unregister_code(KC_NUBS);
+                }
+            } else {
+                if (record->event.pressed) {
+                    register_code(KC_GRV);
+                } else {
+                    unregister_code(KC_GRV);
+                }
+            }
+            break;
+        default:
+            return true;
+    }
+    return false;
+}
 
 void keyboard_post_init_user(void) {
     rgb_matrix_enable();
